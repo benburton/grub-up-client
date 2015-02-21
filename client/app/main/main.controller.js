d3b96ae995a1b99d1babf3b5fe7250947dfc0525
benburton <benjamin.joseph.burton@gmail.com>
2015-02-21 12:15:53 -0500

    Use fallback location

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,15 +6,6 @@ angular.module('grubUpClientApp').controller('MainCtrl', [
   'LocationService',
   function($scope, $timeout, LocationService) {
 
-    $scope.lat = '40.44';
-    $scope.long = '-79.95';
-
-    if (navigator && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(geoData) {
-        console.log(geoData.coords);
-      });
-    }
-
     $scope.$on('mapInitialized', function(event, map) {
       $scope.map = map;
     });
