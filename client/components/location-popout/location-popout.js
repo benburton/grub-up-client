47232ec4adfacb132f4328e33195e8b6b599acc8
benburton <benjamin.joseph.burton@gmail.com>
2015-02-21 16:16:15 -0500

    Responsive popout

--- client/components/location-popout/location-popout.js
+++ client/components/location-popout/location-popout.js
@@ -6,13 +6,14 @@ angular.module('grubUpClientApp').directive('locationPopout', [
       link: function($scope, $element) {
       },
       template: [
-        "<div class='location-popout' ng-class='{expanded: location}'>",
+        "<div class='location-popout'>",
         "  <h2>{{location.name}}</h2>",
         "  <ul class='address'>",
         "    <li>{{location.address}}</li>",
         "    <li>Pittsburgh, PA {{location.zip}}</li>",
         "  </ul>",
         "</div>"].join(''),
+      replace: true,
       scope: {
         location: '='
       }
