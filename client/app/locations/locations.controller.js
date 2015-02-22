4bdc78ebe3e72e585baff5cad3045be2633c2560
benburton <benjamin.joseph.burton@gmail.com>
2015-02-22 00:52:31 -0500

    Add data to locations page

--- client/app/locations/locations.controller.js
+++ client/app/locations/locations.controller.js
@@ -3,7 +3,19 @@
 angular.module('grubUpClientApp').controller('LocationsCtrl', [
   '$rootScope',
   '$scope',
-  function($rootScope, $scope) {
+  'LocationService',
+  function($rootScope, $scope, LocationService) {
     $rootScope.showSearch = false;
+
+    $scope.init = function() {
+      LocationService.getLocations(function(locations) {
+        $scope.locations = _.sortBy(locations, function(location) {
+          return location.name;
+        });
+      });
+    };
+
+    $scope.init();
+
   }
 ]);
